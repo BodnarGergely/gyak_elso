7f6184e3f1e3289e4dd4cf1aebba561b381c4223
Bodnar Gergely <b.gergely1989@gmail.com>
2015-02-04 22:07:34 +0100

    UJ STRUKTURA
    Az elso ag tesztelve, a masodik meg nem.

--- gyak_elso/gyak_elso/elso.c
+++ gyak_elso/gyak_elso/elso.c
@@ -1,15 +1,57 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-main()
+#define CHOICE 1
+
+#if (CHOICE == 1)
+
+void main()
 {
 	char str[100];
 
-	int i = 0;
-	while(i < 15)
+	printf("Ez az elso gyakorlopelda\n\r");
+	printf("Egyszeruen visszairja a kijelzore a beirt stringet. 'exit'-et begépelve kilép. \n\r");
+
+	gets( str );
+
+	while(strcmp(str, "exit") != 0)
+	{
+		printf("Leirom, hogy: %s \n\r", str);
+		gets( str );
+	}
+	printf("Press any key!");
+	getchar();
+}
+
+#elif CHOICE == 2
+void main()
+{
+	char str[100];
+	int i   = 0;
+	int num = 0;
+
+	printf("Ez a masodik gyakorlopelda\n\r");
+	printf("Egyszeruen visszairja a kijelzore a beirt stringet integerkent, ha nem szam jött, hanem szoveg, akkor kilép.\n\r");
+
+	while(1)
 	{
 		gets( str );
-		printf("Leirom, hogy: %s \n\r", str);
-		i++;
+		num = atoi( str );
+
+		if(num == 0)
+		{
+			printf("Exit.");
+			break;
+		}
+
+		else
+		{
+			printf("A bejött adat: %d \n\r", num);
+		}
 	}
-	while(1){}
+	printf("Press any key!");
+	getchar();
 }
+
+#endif
